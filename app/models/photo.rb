153e040834fa002e7209c2d9e72b18ea32b9ef94
rakin <rakinadm19@gmail.com>
2020-04-23 23:12:20 -0700

    Added associations for tags and photos

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -14,5 +14,11 @@ class Photo < ApplicationRecord
     through: :album_inclusions,
     source: :photo_album
 
+  has_many :tagged_photos
+
+  has_many :tags,
+    through: :tagged_photos,
+    source: :tag
+
   has_one_attached :image
 end
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,3 +1,9 @@
 class Tag < ApplicationRecord
   validates :name, presence: true, uniqueness: true
+
+  has_many :tagged_photos
+
+  has_many :photos,
+    through: :tagged_photos,
+    source: :photo
 end

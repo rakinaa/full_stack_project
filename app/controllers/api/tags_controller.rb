d1ff1980b4f095ea0cd995eb982d82694ceaa979
rakin <rakinadm19@gmail.com>
2020-04-26 11:47:36 -0700

    Added params to tag controller and create now check if tag already exists

--- app/controllers/api/tags_controller.rb
+++ app/controllers/api/tags_controller.rb
@@ -1,13 +1,25 @@
 class Api::TagsController < ApplicationController
   def create
-    @tag = Tag.new(tag_params)
-    if @tag.save
+    @tag = Tag.find_by_name(tag_params.name)
+    if @tag
       @tagged_photo = TaggedPhoto.new(photo_id: params.photo_id)
       @tagged_photo.tag_id = @tag.id
       @tagged_photo.save
-      render "api/tags/show"
+      render "api/photos/show"
     else
-      render json: @tag.errors.full_messages, status: 422
+      @tag = Tag.new(name: tag_params.name)
+      if @tag.save
+        @tagged_photo = TaggedPhoto.new(photo_id: params.photo_id)
+        @tagged_photo.tag_id = @tag.id
+        @tagged_photo.save
+        render "api/photos/show"
+      else
+        render json: @tag.errors.full_messages, status: 422
+      end
     end
+  end
+
+  def tag_params
+    params.require(:tag).permit(:name, :photo_id)
   end
 end

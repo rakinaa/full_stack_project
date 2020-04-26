c48e16fa785a64751459340bed6ff5e4659416bf
rakin <rakinadm19@gmail.com>
2020-04-25 22:30:57 -0700

    Added create action to tag controller

--- app/controllers/api/tags_controller.rb
+++ app/controllers/api/tags_controller.rb
@@ -1,2 +1,13 @@
 class Api::TagsController < ApplicationController
+  def create
+    @tag = Tag.new(tag_params)
+    if @tag.save
+      @tagged_photo = TaggedPhoto.new(photo_id: params.photo_id)
+      @tagged_photo.tag_id = @tag.id
+      @tagged_photo.save
+      render "api/tags/show"
+    else
+      render json: @tag.errors.full_messages, status: 422
+    end
+  end
 end

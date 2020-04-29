2c22cb6f96d60732c069c7bf22f744b809e674ec
rakin <rakinadm19@gmail.com>
2020-04-29 10:42:09 -0700

    Fixed file names and added modal data to ui reducer

--- frontend/reducers/ui_reducer.js
+++ frontend/reducers/ui_reducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import modalReducer from './modal_reducer';
+import modalDataReducer from './modal_data_reducer';
 
 const uiReducer = combineReducers({
-  modal: modalReducer
+  modal: modalReducer,
+  modalData: modalDataReducer
 });
 
 export default uiReducer;

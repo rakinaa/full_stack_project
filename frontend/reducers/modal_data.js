af6fd5c3ef0fc659b8cbd0f7bc5fad4cc9f9ec83
rakin <rakinadm19@gmail.com>
2020-04-29 10:34:00 -0700

    Added reducer to pass data to modal

---
+++ frontend/reducers/modal_data.js
@@ -0,0 +1,14 @@
+import { OPEN_MODAL, CLOSE_MODAL } from '../actions/modal_actions';
+
+export default function modalDataReducer(state={}, action) {
+  Object.freeze(state);
+  let newState = Object.assign({}, state);
+  switch (action.type) {
+    case OPEN_MODAL:
+      newState[action.modalData.label] = action.modalData.data;
+    case CLOSE_MODAL:
+      return {};
+    default:
+      return state;
+  }
+}

2729c7b9b06dfc64bd087d57158308f983712281
rakin <rakinadm19@gmail.com>
2020-04-28 22:21:47 -0700

    Added comments reducer to frontend

--- frontend/reducers/comments_reducer.js
+++ frontend/reducers/comments_reducer.js
@@ -1,11 +1,15 @@
-import { RECEIVE_PHOTO } from "../actions/photo_actions";
+import { RECEIVE_COMMENT, REMOVE_COMMENT } from "../actions/comment_actions";
 
 const commentsReducer = (state={}, action) => {
   Object.freeze(state);
   let newState = Object.assign({}, state);
   switch(action.type) {
-    case RECEIVE_PHOTO:
-      return action.payload.comments;
+    case RECEIVE_COMMENT:
+      newState[action.payload.comment.id] = action.payload.comment;
+      return newState;
+    case REMOVE_COMMENT:
+      delete newState[action.payload.comment.id]
+      return newState;
     default:
       return state;
   }

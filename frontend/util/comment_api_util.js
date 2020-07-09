40b54ee41e6c5bcb17d27417574520c69533173e
rakin <rakinadm19@gmail.com>
2020-07-08 22:55:28 -0700

    Added update ajax call to comment util

--- frontend/util/comment_api_util.js
+++ frontend/util/comment_api_util.js
@@ -1,14 +1,20 @@
-export const createComment = comment => (
+export const createComment = (comment) =>
   $.ajax({
-    method: 'POST',
-    url: '/api/comments',
-    data: { comment }
-  })
-);
+    method: "POST",
+    url: "/api/comments",
+    data: { comment },
+  });
 
-export const deleteComment = commentId => (
+export const updateComment = (commentId, commentData) => {
+  return $.ajax({
+    method: "PATCH",
+    url: `/api/comments/${commentId}`,
+    data: commentData,
+  });
+};
+
+export const deleteComment = (commentId) =>
   $.ajax({
-    method: 'DELETE',
-    url: `/api/comments/${commentId}`
-  })
-);
+    method: "DELETE",
+    url: `/api/comments/${commentId}`,
+  });

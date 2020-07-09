0d00dbb236761e35208370cbe2102218321c68cb
rakin <rakinadm19@gmail.com>
2020-07-08 21:11:40 -0700

    Added destroy to photo route

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   namespace :api, defaults: {format: :json} do
     resources :users, only: [:create, :show]
     resource :session, only: [:create, :destroy, :show]
-    resources :photos, only: [:create , :update, :index, :show]
+    resources :photos, only: [:create , :update, :index, :show, :destroy]
     resources :showcase_photos, only: [:index]
     resources :comments, only: [:create, :update, :destroy]
     resources :photo_albums, only: [:create, :update, :destroy, :show]

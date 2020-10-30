4e1357cd280e7cbe596f589d70abbeb99fae693e
rakin <rakinadm19@gmail.com>
2020-10-29 23:09:56 -0700

    reformatted photoindex

--- frontend/components/photos/photo_index.jsx
+++ frontend/components/photos/photo_index.jsx
@@ -1,22 +1,28 @@
-import React from 'react'
-import PhotoIndexItem from './photo_index_item';
+import React from "react";
+import PhotoIndexItem from "./photo_index_item";
 
 class PhotoIndex extends React.Component {
   componentDidMount() {
     this.props.getPhotos();
     window.scrollTo(0, 0);
   }
-  
+
   render() {
     return (
       <div className="constrainer">
         <ul className="index-container">
           {this.props.photos.map((photo) => {
-            return <PhotoIndexItem user={this.props.users[photo.user_id]} key={photo.id} photo={photo} />
+            return (
+              <PhotoIndexItem
+                user={this.props.users[photo.user_id]}
+                key={photo.id}
+                photo={photo}
+              />
+            );
           })}
         </ul>
       </div>
-    )
+    );
   }
 }
 

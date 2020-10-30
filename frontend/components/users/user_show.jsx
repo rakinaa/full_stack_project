98566f074f2c94d1c99fc4326081cf6f3a80313d
rakin <rakinadm19@gmail.com>
2020-10-29 23:46:42 -0700

    Removed unused dependencies from user show

--- frontend/components/users/user_show.jsx
+++ frontend/components/users/user_show.jsx
@@ -1,6 +1,4 @@
 import React from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faGift } from "@fortawesome/free-solid-svg-icons";
 import { Link } from "react-router-dom";
 
 class UserShow extends React.Component {

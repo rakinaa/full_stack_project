e453f3222d32df592c347c9a33e221502e61cf5e
rakin <rakinadm19@gmail.com>
2020-10-30 23:41:41 -0700

    Reformatter userphotoindex

--- frontend/components/albums/user_photo_index.jsx
+++ frontend/components/albums/user_photo_index.jsx
@@ -1,30 +1,41 @@
-import React from 'react'
-import PhotoIndexItem from '../photos/photo_index_item';
+import React from "react";
+import PhotoIndexItem from "../photos/photo_index_item";
 
 class UserPhotoIndex extends React.Component {
   componentDidMount() {
-    this.props.getUser(this.props.match.params.userId)
+    this.props.getUser(this.props.match.params.userId);
   }
 
   render() {
     return (
       <div>
+        {this.props.currentUser.id == this.props.match.params.userId ? (
+          <div className="alert-new-photo">
+            <button
+              onClick={() => this.props.history.push("/photos/new")}
+              className="blue-button"
+            >
+              Post Photos
+            </button>
+          </div>
+        ) : null}
         <div className="user-photo-index">
           <div className="constrainer">
             <ul className="index-container uin-contain">
-              {this.props.currentUser.id == this.props.match.params.userId ?
-                <div className="alert-new-photo">
-                  <button onClick={() => this.props.history.push('/photos/new')} className="blue-button">Post Photos</button>
-                </div> : null
-              }
               {this.props.photos.map((photo) => {
-                return <PhotoIndexItem user={this.props.users[photo.user_id]} key={photo.id} photo={photo} />
+                return (
+                  <PhotoIndexItem
+                    user={this.props.users[photo.user_id]}
+                    key={photo.id}
+                    photo={photo}
+                  />
+                );
               })}
             </ul>
           </div>
         </div>
       </div>
-    )
+    );
   }
 }
 
